328a008e6eade02c297ec3f03185bb13b68290ff
ntlhui <ntlhui@gmail.com>
2015-06-08 10:49:46 -0700

    Added read_rtlsdr function

--- collarTracker/rtlsdr.c
+++ collarTracker/rtlsdr.c
@@ -1,9 +1,22 @@
 #include <rtl-sdr.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-void setup_rtlsdr(rtlsdr_dev_t** dev, unsigned int sampling_frequency,
-                  unsigned int center_frequency, int* gain_val, int cur_gain_idx) {
+#include <stdint.h>
+#include <errno.h>
+/**
+ * Sets up the RTL SDR with the given parameters.  Accepts a pointer to a local
+ * variable of type rtlsdr_dev_t*, and will populate that variable with the
+ * address of the rtlsdr_dev_t struct.  Accepts as unsigned 32-bit integers the
+ * sampling frequency and center frequency.  Accepts as a signed 32-bit integer
+ * the initial gain.
+ *
+ * @param dev                device variable
+ * @param sampling_frequency Sampling frequency as an unsigned 32-bit int
+ * @param center_frequency   Center frequency as an unsigned 32-bit int
+ * @param gain           initial gain as a signed 32-bit int
+ */
+void setup_rtlsdr(rtlsdr_dev_t** dev, uint32_t sampling_frequency,
+                  uint32_t center_frequency, int gain) {
 	// setup rtlsdr
 	if (!rtlsdr_get_device_count()) {
 		fprintf(stderr, "No supported devices found! Exiting...\n");
@@ -28,22 +41,51 @@ void setup_rtlsdr(rtlsdr_dev_t** dev, unsigned int sampling_frequency,
 		fprintf(stderr, "WARNING: Failed to enable manual gain!\n");
 	}
 
-	if (rtlsdr_set_tuner_gain(*dev, gain_val[cur_gain_idx])) {
-		fprintf(stderr, "WARMING: Failed to set fixed gain!\n");
+	if (rtlsdr_set_tuner_gain(*dev, gain) {
+	fprintf(stderr, "WARMING: Failed to set fixed gain!\n");
 	}
 
 	if (rtlsdr_reset_buffer(*dev)) {
-		fprintf(stderr, "WARNING: Failed to reset buffers!\n");
+	fprintf(stderr, "WARNING: Failed to reset buffers!\n");
 	}
 }
 
+/**
+ * Fills the given buffer with data from the given RTLSDR device.
+ *
+ * @param  dev             RTL SDR device
+ * @param  time_buffer     buffer to fill with data
+ * @param  time_buffer_len length of buffer
+ * @return                 0 on success, -1 otherwise
+ */
+int read_rtlsdr(rtlsdr_dev_t* dev, uint8_t* time_buffer, int time_buffer_len) {
+	int n_read = 0;
+
+	if (rtlsdr_read_sync(dev, time_buffer, time_buffer_len, &n_read)) {
+		fprintf(stderr, "WARNING: sync read failed!\n");
+		return -1;
+	}
+
+	if (n_read < time_buffer_len) {
+		fprintf(stderr, "WARNING: samples lost!\n");
+		return -1;
+	}
+	return 0;
+}
+
+/**
+ * Tests the given RTL SDR for reads.
+ *
+ * @param  dev Device to test
+ * @return     0 on success, -1 otherwise.
+ */
 int test_rtlsdr(rtlsdr_dev_t* dev) {
 	int n_read;
-	int* time_buffer = malloc(256);
+	uint8_t* time_buffer = malloc(256);
 	if (!time_buffer) {
 		return -1;
 	}
-	int time_buffer_len = 64;
+	int time_buffer_len = 256;
 	// TODO: set short delay of tens of samples
 	rtlsdr_read_sync(dev, time_buffer, time_buffer_len, &n_read);
 	if (n_read == 0) {
--- collarTracker/rtlsdr.h
+++ collarTracker/rtlsdr.h
@@ -1,7 +1,37 @@
 #ifndef __RTLSDR_MOD__
 #define __RTLSDR_MOD__
 #include <rtl-sdr.h>
+/**
+ * Sets up the RTL SDR with the given parameters.  Accepts a pointer to a local
+ * variable of type rtlsdr_dev_t*, and will populate that variable with the
+ * address of the rtlsdr_dev_t struct.  Accepts as unsigned 32-bit integers the
+ * sampling frequency and center frequency.  Accepts as a signed 32-bit integer
+ * the initial gain.
+ *
+ * @param dev                device variable
+ * @param sampling_frequency Sampling frequency as an unsigned 32-bit int
+ * @param center_frequency   Center frequency as an unsigned 32-bit int
+ * @param gain           initial gain as a signed 32-bit int
+ */
+void setup_rtlsdr(rtlsdr_dev_t** dev, uint32_t sampling_frequency,
+                  uint32_t center_frequency, int gain);
 
-void setup_rtlsdr(rtlsdr_dev_t**, unsigned int, unsigned int, int*, int);
-int test_rtlsdr(rtlsdr_dev_t*);
+/**
+ * Tests the given RTL SDR for reads.
+ *
+ * @param  dev Device to test
+ * @return     0 on success, -1 otherwise.
+ */
+int test_rtlsdr(rtlsdr_dev_t* dev);
+
+/**
+ * Fills the given buffer with data from the given RTLSDR device.
+ *
+ * @param  dev             RTL SDR device
+ * @param  time_buffer     buffer to fill with data
+ * @param  time_buffer_len length of buffer
+ * @return                 0 on success, -1 otherwise
+ */
+int read_rtlsdr(rtlsdr_dev_t* dev, uint8_t* time_buffer,
+                int time_buffer_len);
 #endif //__RTLSDR_MOD__
