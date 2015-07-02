296aa26ba0745a81ffe04190613aa295335357a6
ntlhui <ntlhui@gmail.com>
2015-07-01 18:13:36 -0700

    switched to counting files on SD card

--- autostart/parser.sh
+++ autostart/parser.sh
@@ -6,27 +6,25 @@ timestamp(){
 if [ ! -e /sys/class/gpio/gpio60 ]
 	then
 	echo 60 > /sys/class/gpio/export
+	echo out > /sys/class/gpio/gpio60/direction
 fi
 run=true
 threshold="35"
 log="/home/debian/rctparser.log"
 while $run; do
-	sleep 5
-	modify=$(date -r out.tmp +%s)
-	current=$(date +%s)
-	diff=$(($current - $modify))
-
-	if [[ $diff -gt $threshold ]]
+	numFiles=$(ls -l /media/RAW_DATA/rct/ | wc -l)
+	sleep 30
+	if [[ $(ls -l /media/RAW_DATA/rct/ | wc -l) -ne numFiles ]]
 		then
-		echo low > /sys/class/gpio/gpio60/direction
+		echo "0" > /sys/class/gpio/gpio60/direction
 		run=false;
 		sudo kill -9 `pgrep collarTracker`
 		echo "$(timestamp): Killing collarTracker"
 		echo "$(timestamp): Killing collarTracker" >> $log
 	else
-		echo high > /sys/class/gpio/gpio60/direction
+		echo "1" > /sys/class/gpio/gpio60/direction
 		echo "$(timestamp): collarTracker is still alive"
 		echo "$(timestamp): collarTracker is still alive" >> $log
 	fi
 done
-echo low > /sys/class/gpio/gpio60/direction
+echo "0" > /sys/class/gpio/gpio60/direction
