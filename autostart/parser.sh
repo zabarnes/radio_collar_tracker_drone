51ad9c32d407e59ab7d52f3ec2bcdcc8249cc5f5
ntlhui <ntlhui@gmail.com>
2015-07-01 18:43:56 -0700

    Fixed LED files

--- autostart/parser.sh
+++ autostart/parser.sh
@@ -6,24 +6,24 @@ timestamp(){
 if [ ! -e /sys/class/gpio/gpio60 ]
 	then
 	echo 60 > /sys/class/gpio/export
-	echo out > /sys/class/gpio/gpio60/direction
 fi
+echo out > /sys/class/gpio/gpio60/direction
 run=true
 threshold="35"
 log="/home/debian/rctparser.log"
 while $run; do
 	if [[ -z $(pgrep collarTracker) ]]
 		then
-		echo "0" > /sys/class/gpio/gpio60/direction
+		echo "0" > /sys/class/gpio/gpio60/value
 		run=false;
 		sudo kill -9 `pgrep collarTracker`
 		echo "$(timestamp): Killing collarTracker"
 		echo "$(timestamp): Killing collarTracker" >> $log
 	else
-		echo "1" > /sys/class/gpio/gpio60/direction
+		echo "1" > /sys/class/gpio/gpio60/value
 		echo "$(timestamp): collarTracker is still alive"
 		echo "$(timestamp): collarTracker is still alive" >> $log
 	fi
 	sleep 1
 done
-echo "0" > /sys/class/gpio/gpio60/direction
+echo "0" > /sys/class/gpio/gpio60/value
